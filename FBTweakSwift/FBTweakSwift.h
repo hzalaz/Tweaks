82e657353caf9b1b86edbb290363a3bccaf0fbfd
Hernan Zalazar <hernanzalazar@gmail.com>
2015-12-10 17:20:07 -0300

    Add header to umbrella

--- FBTweakSwift/FBTweakSwift.h
+++ FBTweakSwift/FBTweakSwift.h
@@ -17,3 +17,14 @@ FOUNDATION_EXPORT const unsigned char FBTweakSwiftVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <FBTweakSwift/PublicHeader.h>
 
 
+#import <FBTweakSwift/FBTweak.h>
+#import <FBTweakSwift/FBTweakCollection.h>
+#import <FBTweakSwift/FBTweakCategory.h>
+#import <FBTweakSwift/FBTweakStore.h>
+#import <FBTweakSwift/FBTweakEnabled.h>
+#import <FBTweakSwift/FBTweakInline.h>
+#import <FBTweakSwift/FBTweakInlineInternal.h>
+#import <FBTweakSwift/_FBTweakBindObserver.h>
+#import <FBTweakSwift/FBTweakShakeWindow.h>
+#import <FBTweakSwift/FBTweakViewController.h>
+#import <FBTweakSwift/FBColorUtils.h>
